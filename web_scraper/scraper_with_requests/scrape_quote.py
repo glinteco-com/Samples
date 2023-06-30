5cec769fd408d0d0a98e1f085d0208caeaba0a67
hoavq-glinteco <hoa.vuquang@glinteco.com>
2023-06-30 22:47:40 +0700

    Add scrape quote

--- web_scraper/scraper_with_requests/scrape_quote.py
+++ web_scraper/scraper_with_requests/scrape_quote.py
@@ -1,32 +1,69 @@
-import json
+# Python 3.11
+
+import argparse
+import csv
 
 import requests
 from bs4 import BeautifulSoup
 
+BASE_URL = "https://quotes.toscrape.com"
 
-def scrape_site(url):
-    # Send an HTTP request to the URL of the webpage you want to access
+
+def scrape_url(url):
+    print(f"Scrape url {url}")
     response = requests.get(url)
 
-    # Parse the HTML content using Beautiful Soup
-    soup = BeautifulSoup(response.content, "html.parser")
+    if response.status_code != 200:
+        breakpoint()
 
-    quote_divs = soup.find_all("div", attrs={"class": "quote"})
+    soup = BeautifulSoup(response.text, "html.parser")
+    quote_divs = soup.select(".quote")
+
     quotes = []
     for quote_div in quote_divs:
-        quote_content = quote_div.find("span", attrs={"class": "text"}).string[
-            1:-1
-        ]
-        quote_tags = [
-            tag_div.string
-            for tag_div in quote_div.find_all("a", attrs={"class": "tag"})
-        ]
-        quotes.append({"content": quote_content, "tags": quote_tags})
+        text = quote_div.select_one(".text").get_text()
+        if '"' in text:
+            text = text.replace('"', "")
+        if "“" in text:
+            text = text.replace("“", "")
+        if "”" in text:
+            text = text.replace("”", "")
+        author = quote_div.select_one(".author").get_text()
+        quote = {"text": text, "author": author}
+        quotes.append(quote)
 
-    return quotes
+    print(f"Found #{len(quotes)} quotes")
+
+    next_url = None
+    if soup.select_one(".next > a"):
+        next_link = soup.select_one(".next > a")["href"]
+        next_url = f"{BASE_URL}{next_link}"
+
+    return next_url, quotes
+
+
+def write_to_file(file_name, quotes):
+    field_names = ["author", "text"]
+    with open(file_name, "w") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=field_names)
+        writer.writeheader()
+        writer.writerows(quotes)
 
 
 if __name__ == "__main__":
-    url = "http://quotes.toscrape.com/page/1/"
-    quotes = scrape_site(url)
-    print(json.dumps(quotes, indent=4))
+    parser = argparse.ArgumentParser(description="Scrape quotes")
+    parser.add_argument("-f", "--file_name")
+    args = parser.parse_args()
+
+    file_name = "quotes.csv"
+    if args.file_name:
+        file_name = args.file_name
+
+    print("Start scraping")
+    next_url = "https://quotes.toscrape.com/"
+    quotes = []
+    while next_url:
+        next_url, new_quotes = scrape_url(next_url)
+        quotes.extend(new_quotes)
+    print(f"Total found #{len(quotes)}")
+    write_to_file(file_name, quotes)
