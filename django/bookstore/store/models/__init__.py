4e10d529468aaa8191de6989b95d9f7464c8b273
dat.ngoquang <dat.ngoquang@glinteco.com>
2024-08-23 22:09:03 +0700

    Django site framework demo

--- django/bookstore/store/models/__init__.py
+++ django/bookstore/store/models/__init__.py
@@ -2,10 +2,12 @@ from .author import Author
 from .book import Book
 from .category import Category
 from .publisher import Publisher
+from .article import Article
 
 __all__ = [
     "Publisher",
     "Book",
     "Author",
     "Category",
+    "Article",
 ]
